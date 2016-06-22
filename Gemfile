e2cefe7077cfc9d6a6ecaa2d7058bbae633ecca9
KateSTento <bktento@skymesh.com.au>
2016-06-22 11:15:30 +0000

    added stripe intergration

--- Gemfile
+++ Gemfile
@@ -50,6 +50,8 @@ gem "fog"
 gem 'mini_magick', '~> 4.3.1'
 
 gem 'carrierwave-aws'
+
+gem 'stripe'
 # Use ActiveModel has_secure_password
 # gem 'bcrypt', '~> 3.1.7'
 
--- app/controllers/lessons_controller.rb
+++ app/controllers/lessons_controller.rb
@@ -1,8 +1,21 @@
 class LessonsController < ApplicationController
+  before_action :authenticate_user!
+  before_action :require_course_enrollment_for_lessons, only: [:show]
+
   def show
+    
   end
 
   private
+
+  def require_course_enrollment_for_lessons
+      the_course = current_lesson.section.course
+
+      if !current_lesson.section.course.user.enrolled_in?(the_course)
+        redirect_to course_path(the_course), alert: 'Please enroll to access lessons'
+      end
+    end
+    
   helper_method :current_lesson
   def current_lesson
     @current_lesson ||= Lesson.find(params[:id])
